32d5374ad20fdbdc8a0266e6f3bb69dbcd97ace3
Kritarat.M <ideal141148@gmail.com>
2024-06-14 19:37:53 +0700

    add views

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 const path = require("path");
 
 app.use(express.static(path.join(__dirname,"/public/")));
+
+app.set("views","./src/views");
+app.set("views engine","ejs");
 
 app.get("/", (req, res) => {
     res.send("Hello world");
