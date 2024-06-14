68f7fa42c7fc1b54de16b5500de77694fe69d807
Kritarat.M <ideal141148@gmail.com>
2024-06-14 19:03:45 +0700

    add/test public folder

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req, res) => {
     res.send("Hello world");
