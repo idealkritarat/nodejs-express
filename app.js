4499176c8e2000eb202f573b51c6cc04edaf6b43
Kritarat.M <ideal141148@gmail.com>
2024-06-14 20:31:32 +0700

    add products.ejs

--- app.js
+++ app.js
@@ -2,16 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const path = require("path");
+const productRouter = express.Router();
 
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","./src/views");
-app.set("view engine","ejs");
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index",{username: 'Kritarat',customer:["Hello","Hey","hi"]})
+
+app.use("/products", productRouter);
+
+productRouter.route("/").get((req, res) => {
+    res.send("Hello world !! I'm Product");
 });
 
-app.listen(PORT, ()=> {
-    console.log("Listening on port %d",PORT);
-})
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello world !! I'm Product1");
+});
+
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        username: "Kritarat",
+        customer: ["Hello", "Hey", "hi"],
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Listening on port %d", PORT);
+});
